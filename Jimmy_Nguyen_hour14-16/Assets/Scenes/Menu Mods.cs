7f8f452b1662dd0fdb46acc461e163014c7371a7
waterhulk09 <hulkjimmy123@yahoo.com>
2026-05-05 22:46:27 -0700

    Made particles and remade in a way ZR main menu

---
+++ Jimmy_Nguyen_hour14-16/Assets/Scenes/Menu Mods.cs
@@ -0,0 +1,21 @@
+/* Changes in the menu.
+  
+This is a recreation of my old project Zenith Rising (ZR) on roblox (menu wise at least)
+
+I added image buttons instead of the normal buttons with text on them 
+
+The transitions I used were images, IE images that darken when selected and text that would become yellow when hovered over.
+
+The background image is of what the game is meant to look like showing all of the characters (this was a screen shot taken in engine in a testing place)
+
+I added some background music (this is the same music I used in the original version as well)
+
+I added some minor details like my game studio logo and version number just for some flavor.
+
+Instead of using text the title is an image showing the games text logo.
+ 
+ 
+ 
+ 
+ 
+ */
---
+++ Jimmy_Nguyen_hour14-16/Assets/Scenes/Particals.cs
@@ -0,0 +1,11 @@
+/*
+
+What I did is changed the start time to 0.2, changed the start speed to 1.74, the size 8.53, and the sim speed to 2.
+
+I changed the shape to be a box that emits from the edge with a position of 0,-0.1,0 and a scale of 1,1,2.8
+
+I changed the emission rate to 80 and set the textures alpha cut off value to 0.544.
+
+I also added a color over time and I changed the image texture to be fade rendering mode as it looked wonky using transparent and cutout had a weird thick outline
+ 
+ */
